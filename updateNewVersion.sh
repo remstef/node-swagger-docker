71d3a34aa7db99d1155fc461aaa5b70f8ebea5c9
Steffen Remus <steremus@gmail.com>
2020-10-21 21:55:32 +0200

    update script

--- updateNewVersion.sh
+++ updateNewVersion.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
+if [ -z $1 ]
+then
   echo Please specify a version number! ... cancelling
   exit 1
 fi
 
 v=${1}
+
+if ( git tag --list | grep ^${v}$; )
+then
+  echo "Tag already exists. Do you want to overwrite it?"
+  read -p "[Y/N]: " decision
+  if ! [[ ${decision} = "Y" || ${decision} = "y" ]]
+  then
+    echo "... cancelling"
+    exit 1
+  fi
+  echo "Removing local tag..."
+  git tag -d "${v}"
+  echo "Removing remote tag..."
+  git push origin ":refs/tags/${v}"
+fi
 
 echo "Updating nodejs version to ${v}"
 
@@ -15,10 +31,9 @@ cat Dockerfile.template | sed s/VERSION/${v}/ > Dockerfile
 echo "Creating tag..."
 git tag -f "${v}"
 
-echo "Removing remote tag (if it existed)..."
-git push origin ":refs/tags/${v}"
-
 echo "Push tags..."
 git push --tags
 
-echo "Pushed new docker version. Please update docker hub references!"
+echo "Pushed new docker version. Please check automated builds in docker hub manually!"
+echo "If necessary, manually run:"
+echo " "curl -H \"Content-Type: application/json\" --data \"{\'source_type\': \'Tag\', \'source_name\': \'${v}\'}\" -X POST https://registry.hub.docker.com/u/remstef/node-swagger/trigger/e3846c99-664e-473a-a10f-ad8e9bce4aaa/
